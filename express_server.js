86e9092fe420a61a022622b4f9a159000ede962b
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-11 04:25:39 +0000

    created new urls page and installed body parser, then added post request that gives dummy ok response

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 8080; // default port 8080
 //const morgan = require('morgan'); // let express know to use middlewaree
 //app.use(morgan('dev'));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); // Set ejs as view engine
 
 const urlDatabase = {
@@ -18,6 +20,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -30,6 +36,11 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
