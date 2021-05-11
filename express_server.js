b86e42688eccc4f93b0edc2a7b373bed5b6097f7
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-11 06:11:58 +0000

    added redirect to shorturl page when new url is created and added redirect to longurl when short url page is requested

--- express_server.js
+++ express_server.js
@@ -40,14 +40,20 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);// Log the POST request body to the console
-  urlDatabase[generateRandomString()] = req.body['longURL']; 
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let newShortURL =generateRandomString();
+  urlDatabase[newShortURL] = req.body['longURL']; 
+  res.redirect(`/urls/:${newShortURL}`);         
 });
 
 app.listen(PORT, () => {
