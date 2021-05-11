426b0eda8654a41a464e94ed1066f340eca0bcee
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-11 20:40:42 +0000

    creating a delete coloumn and button, and then setting up and POST request to delete tthe key value pair from urlDatabases

--- express_server.js
+++ express_server.js
@@ -20,44 +20,64 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//Home Page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//Hello Page
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//Add New Urls page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Urls index Page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//specifc short url page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(templateVars);
   res.render("urls_show", templateVars);
 });
 
+//redirects short url to long url
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//retruns urlDatabase a json object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//Creates new short url for entered long url and saves it to the urlDatabase
+//calls generaterandomstring to create new short url. the shorturl and long url are added as key value pair 
+//then redirects to the new shorturl page
 app.post("/urls", (req, res) => {
-  console.log(req.body);// Log the POST request body to the console
+  console.log(req.body);
   let newShortURL =generateRandomString();
   urlDatabase[newShortURL] = req.body['longURL']; 
-  res.redirect(`/urls/:${newShortURL}`);         
+  res.redirect(`/urls/${newShortURL}`);         
 });
 
+//deletes shorturl and longurl key value pair from the urlDatabase object
+//and redirects back to the url index page
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let urlToDelete = req.params['shortURL'];
+  delete urlDatabase[urlToDelete];
+  res.redirect(`/urls`);         
+});
+
+//message indicated server is running
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
