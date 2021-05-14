027214f21a6a7c8b437989c29143e2cab79f33b6
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-14 05:44:15 +0000

    Fixing minor errors

--- express_server.js
+++ express_server.js
@@ -89,7 +89,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) { res.sendStatus(404); }
 
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL]['longURL'];
   res.redirect(longURL);
 });
 
